0dad3a45380c3573b110bcb4062bc6b21c6547bb
Zevs <piszczatowski.artur@gmail.copm>
2022-06-22 00:50:38 +0200

    Setup

---
+++ include/Game.h
@@ -0,0 +1,25 @@
+#ifndef SDL_PROJECT_GAME_H
+#define SDL_PROJECT_GAME_H
+#include <SDL2/SDL.h>
+#include <iostream>
+
+class Game {
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int x, int y, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() { return isRunning; };
+
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+#endif //SDL_PROJECT_GAME_H
